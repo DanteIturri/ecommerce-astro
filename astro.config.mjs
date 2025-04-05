ebb09d4596a39e47217a40aba9fb46cd4869d8f2
Dante Iturri <danteiturri@gmail.com>
2025-04-05 20:44:16 -0300

    feat: Add product image and integrate API for product data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
